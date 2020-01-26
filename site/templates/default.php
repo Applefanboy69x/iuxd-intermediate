4b5a53658dc8f81e47ebc029d86bd911a4ef465b
Applefanboy69x <48945038+Applefanboy69x@users.noreply.github.com>
2020-01-26 15:47:05 +0100

    KIRBY
    F#ck F#ck F#ck F#ck
    En het werkt nog steeds maar half
    (heiligboontje smiley)

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,24 +1,201 @@
 <!doctype html>
 <html lang="en">
   <head>
+
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    
+    <!-- Bootstrap core CSS -->
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-    <title><?= $page->title() ?></title>
-  </head>
+    <!-- Custom styles for this template -->
+    <link href="assets/css/scrolling-nav.css" rel="stylesheet">
+
+
+
+        <!-- Custom JavaScript for this theme -->
+        <script src="assets/js/scrolling-nav.js"></script>
+
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+  <title>Micro-Interactions met Principle</title>
+
+  <link href="https://fonts.googleapis.com/css?family=Calistoga&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Roboto+Slab&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="assets/css/stylesheet.css">
+
+</head>
   <body>
-    <h1><?= $page->title() ?></h1>
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+  <div class="container">
+    <a class="navbar-brand js-scroll-trigger" href="#page-top">Micro-Interactions</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#about">Waar komen de ze vandaan?</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#services">Waarom gebruik je ze?</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#contact">Correct toepassen</a>
+        </li>
+                  <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#contact2">Principle</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 
-    <p><?= $page->text()->kirbytext() ?></p>
+<header id="p" >
+  <div class="container text-center">
+    <h2><?= $page->titel() ?></h2>
+    <p class="lead"><p class="p"><?= $page->sub_titel() ?></p></p>
+  </div>
+</header>
+
+<section id="about">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8 mx-auto">
+        <h3><?= $page->waar_titel() ?></h3>
+        <p class="lead"><?= $page->waar_tekst_intro() ?> </p>
+</p>
+<p class='pppp'><?= $page->waar_tekst() ?></p>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section id="services" class="bg-light">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8 mx-auto">
+        <h3>Waarom gebruik je micro-interactions?</h3>
+        <p class="lead">Micro-interactions zijn maar hele kleine elementen in het designen van een product. Waarom zou je überhaupt aandacht aan ze moeten besteden? <br> Helaas is dat een vraag die veel designers nog stellen. Maar het aandacht voor detail is wat het verschil kan maken tussen een matig of goed product.
+
+Dit zijn redenen waarom micro-interactions waardevol zijn:
+<ul>
+<li> Ze helpen bij het navigeren van je website. </li>
+<li> Ze maken het voor gebruikers makkelijker met je website te interacteren. </li>
+<li> Ze zorgen voor directe feedback met betrekking tot het volbrengen van een taak/actie. </li>
+<li> Ze maken de user experience de moeite waard. </li>
+<li> Ze moedigen delen, liken en reageren op je content aan. </li>
+<li> Ze sturen de aandacht van de gebruiker. </li>
+<li> Ze voegen emotie toe aan je product. </li>
+</ul>
+<br>
+(image: iu2.gif)
+Goed ontworpen micro-interactions zijn een teken dat je als ontwerper geeft om je gebruiker. Daarom zijn ze zo van waarde. De gebruiker snapt wat hij moet doen, en als dat niet lukt kan een app of website feedback geven en de gebruiker helpen. Micro-interactions kunnen positieve gevoelens opwekken over je merk en kunnen je gebruikers’ gedrag beïnvloeden, zonder dat mensen het door hebben.
+</p>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section id="contact">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8 mx-auto">
+        <h3>Correct toepassen van micro-interactions</h3>
+        <p class="lead">Het maken van micro-interactions is belangrijk voor ontwerpers, omdat het ze de mogelijkheid bied om te experimenteren met nieuwe design oplossingen en te kijken naar nieuwe manieren om hun gebruikers te verrassen. 
+
+Micro-interactions bestaan uit vier delen: De trigger, rules, feedback en lopes & modes. 
+De trigger begint de micro-interaction. Triggers kunnen user-initiated (de gebruiker begint de actie) of system-initiated (het system begint de actie) zijn. Rules, deze bepalen wat er precies gaat gebeuren. De feedback laat mensen weten er gaande is. Alles dat de gebruiker ziet, hoort of voelt is feedback. Loops & Modes bepalen wat er gebeurt zodra de situaties veranderd. 
+
+Er zijn echter een paar belangrijke dingen om te onthouden: 
+<ul>
+<li> Verplaats jezelf in de gebruiker en gebruik die inzichten om eerst zelf te begrijpen hoe jouw applicatie werkt. </li>
+<li> Animaties moeten een functie hebben. Ze kunnen er leuk uit zien, maar tijdens het ontwerpen van micro-interactions is functionaliteit belangrijker. </li>
+<li> Entertain je gebruikers. Wat de gebruiker voelt wanneer hij de app gebruikt, is de reden achter het feit dat hij deze blijft gebruiken. Als de gebruiker van de ervaring geniet en als prettig ervaart keert hij terug.</li>
+<li> Let op dat je niet vervelend wordt, teveel animaties kan het tegenovergestelde effect hebben op gebruikers. </li>
+<li> Gebruik liever een menselijke taal dan een technische. Een foutmelding op een website kan heel vervelend zijn, maar kan leuker worden gemaakt door middel van een leuke animatie. </li>
+</ul>
+</p>
+(image: iu1.gif)
+      </div>
+    </div>
+  </div>
+</section>
+
+<section id="contact2" class="bg-light">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8 mx-auto">
+        <h3>Micro-interacties maken met Principle</h3>
+        <p class="lead">Micro-interactions zijn van belang om de gebruiker meer duidelijkheid over je product of dienst te geven. Vooral als je gaat testen kan het maken van een high-end protoytpe in Principle veel toevoegen, het verteld veel over de flow van je product.
+
+Principle is een ideaal programma om snel mooie prototypes te maken. Het verschil met andere programma’s is dat je in Principle iets in 5 minuten kunt maken dat je in een ander programma 45 minuten kan kosten.(kuch invision studio) Hierdoor wordt het ook veel leuker om te gaan experimenteren. Als je een Sketch of Figma gebruiker bent, is het helemaal handig. Ten eerste lijken de lay-outs van de programma’s erg op elkaar en ten tweede werken de programma’s erg goed samen. Als je een bestand open hebt staan in Sketch kun je het heel makkelijk importen in Principle en herkent hij je layers en vormen waarbij hij de gelijknamige elementen tussen artboards koppelen.
+Met Principle kun je heel gemakkelijk de schermen uit Sketch of Figma importeren om een prototypen te maken. 
+
+Als je je artboard hebt aangemaakt, kun je beginnen met het importen van je bestand. Dit kun je doen door ze direct vanuit je Finder in Principle te slepen of ze uit Sketch te importeren. Selecteer de lagen die je wilt importeren en gebruik cmd+C en cmd+V. Houd er wel rekening mee dat als je meerdere lagen tegelijk kopieert en plakt in Principle dat hij er één laag van maakt. Zorg er dus voor dat je de lagen los kopieert en plakt als je dat niet wilt. Als je je bestand de helft zo klein wilt importeren, zet dan @2x in de bestandsnaam. 
+
+
+</p>
+<p class="pp">Onze eerste voorbeeld uitwerking.</p>
+
+<div class="embed-responsive embed-responsive-16by9">
+<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/Z7hQlLygaGk&feature=youtu.be" allowfullscreen></iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/Z7hQlLygaGk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
+
+<p class="ppp">
+Nadat je je bestanden geïmporteerd hebt, is het tijd om te beginnen. Selecteer een element om er vervolgens een actie aan te koppelen. Momenteel heeft Principle de volgende acties: Tap, Long Press, Scroll Begin, Scroll Release, Scroll End, Drag Begin, Drag End, Touch Down, Touch Up, Hover Inside, Hover Outside en Auto. Dupliceer je huidige artboard en koppel een actie ertussen. 
+
+</p>
+
+<p class="pp">Onze tweede voorbeeld uitwerking.</p>
+<div class="embed-responsive embed-responsive-16by9">
+<iframe class="embed-responsive-item" src="https://www.youtube.com/watch?v=vWUPmMfmcP4&feature=youtu.be" allowfullscreen></iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/vWUPmMfmcP4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
+<p class="ppp">Principle is een goede tool voor het maken van interactieve UI prototypes voor je telefoon. Je kunt er heel veel toffe dingen mee maken: mooie interactions, korte animaties, en zelfs apps met meerdere schermen. Principle is makkelijk in gebruik, werkt snel en makkelijk te delen met je klanten en collega’s.
+
+Principle is alleen beschikbaar voor Mac. Het programma kost $129 inclusief een jaar lang gratis updates. Ook geven ze de mogelijkheid om het programma eerst te proberen voor 14 dagen om te kijken of het wat voor jou is. In Principle kun je ontwerpen maken voor Android, iOS en websites of zelfs simpele GIFs. 
+</p>
+
+</section>
+<section id="contact2" class="custom-lars">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8 mx-auto">
+        <h2>Groetjes,</h2>
+        <p class="lead"><p class="p">Bedankt voor het bekijken van onze intro pagina over micro-interacties, jullie feedback is van harte welkom op het E-mailadres: 0909238@hr.nl
+
+</p></p>
+      </div>
+    </div>
+  </div>
+</section>
+    <!-- Footer -->
+      <footer class="py-5 bg-dark">
+        <div class="container">
+          <p class="m-0 text-center text-white">Designing the user experience 2019 - 2020</p>
+        </div>
+      <!-- /.container -->
+    </footer>
+    <!-- Plugin JavaScript -->
+    <script src="assets/vendor/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  
   </body>
 </html>
